cd69dcc5ed25cc045392294e15a3f8be28f7b52d
Samir Parikh <siparikh@gmail.com>
2026-02-01 14:45:52 -0500

    extend test coverage for Weird Algorithm

--- src/CSES.Solutions/Introductory/WeirdAlgorithm/WeirdAlgorithmTests.cs
+++ src/CSES.Solutions/Introductory/WeirdAlgorithm/WeirdAlgorithmTests.cs
@@ -13,6 +13,17 @@ public class WeirdAlgorithmTests : BaseSolverTests<WeirdAlgorithmSolver>
     [InlineData(1)]
     [InlineData(2)]
     [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    [InlineData(10)]
+    [InlineData(11)]
+    [InlineData(12)]
+    [InlineData(13)]
+    [InlineData(14)]
     public void Test(int testNumber)
     {
         RunTest(TestDataFolder, testNumber);
