5e5c50716e8b038f8a6dfa078a8ffe67eb7012ba
Samir Parikh <siparikh@gmail.com>
2026-02-01 17:27:08 -0500

    initial commit for MissingNumber problem

---
+++ src/CSES.Solutions/Introductory/MissingNumber/MissingNumberSolver.cs
@@ -0,0 +1,19 @@
+using CSES.Core;
+
+namespace CSES.Solutions.Introductory;
+
+/// <summary>
+/// Solver for CSES "Weird Algorithm" problem.
+/// Problem: https://cses.fi/problemset/task/1083
+/// You are given all numbers between 1,2,\ldots,n except one. Your task is to
+/// find the missing number.
+/// </summary>
+public class MissingNumberSolver : ISolver
+{
+    public string Solve(string input)
+    {
+
+        string sequence = input;
+        return "1";
+    } 
+}
