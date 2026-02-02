63a33f24bd445d3cda2c88b3a28b729098aecc43
Samir Parikh <siparikh@gmail.com>
2026-02-02 16:33:52 -0500

    fix MissingNumber solver implementation and enable additional test cases

--- src/CSES.Solutions/Introductory/MissingNumber/MissingNumberSolver.cs
+++ src/CSES.Solutions/Introductory/MissingNumber/MissingNumberSolver.cs
@@ -3,17 +3,22 @@ using CSES.Core;
 namespace CSES.Solutions.Introductory;
 
 /// <summary>
-/// Solver for CSES "Weird Algorithm" problem.
+/// Solver for CSES "Missing Number" problem.
 /// Problem: https://cses.fi/problemset/task/1083
-/// You are given all numbers between 1,2,\ldots,n except one. Your task is to
+/// You are given all numbers between 1,2,n except one. Your task is to
 /// find the missing number.
 /// </summary>
 public class MissingNumberSolver : ISolver
 {
     public string Solve(string input)
     {
-
-        string sequence = input;
-        return "1";
+        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        int n = int.Parse(lines[0]);
+        var numbers = lines[1].Split(' ').Select(int.Parse).ToArray();
+        var set = new HashSet<int>(numbers);
+        
+        for (int i = 1; i <= n; i++)
+            if (!set.Contains(i)) return i.ToString();
+        return "0";
     } 
 }
