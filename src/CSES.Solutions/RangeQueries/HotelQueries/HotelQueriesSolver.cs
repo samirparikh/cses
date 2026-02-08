d0def92f4a9d6c7e85a6479787c7bd0b69fe35f0
Samir Parikh <siparikh@gmail.com>
2026-02-07 22:00:32 -0500

    partially working solution for first 3 tests of hotel queries

--- src/CSES.Solutions/RangeQueries/HotelQueries/HotelQueriesSolver.cs
+++ src/CSES.Solutions/RangeQueries/HotelQueries/HotelQueriesSolver.cs
@@ -25,18 +25,59 @@ namespace CSES.Solutions.RangeQueries;
 /// </summary>
 public class HotelQueriesSolver : ISolver
 {
+    private int MaxValue(int[] arr)
+    {
+        int max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        return max;
+    }
+
     public string Solve(string input)
     {
         var lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
         var firstLineParts = lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        int numCoins = int.Parse(firstLineParts[0]);
-        int desiredSum = int.Parse(firstLineParts[1]);
+        int numHotels = int.Parse(firstLineParts[0]);
+        int numGroups = int.Parse(firstLineParts[1]);
 
-        int[] coins = lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        int[] freeRooms = lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int[] groupSizes = lines[2].Split(' ', StringSplitOptions.RemoveEmptyEntries)
             .Select(int.Parse)
             .ToArray();
 
-        return "-1";
+        int[] assignedHotels = new int[numGroups];
+
+        Console.WriteLine($"Number of hotels: {numHotels}, Number of groups: {numGroups}");
+        Console.WriteLine($"Free rooms in hotels: {string.Join(", ", freeRooms)}");
+        Console.WriteLine($"Group sizes: {string.Join(", ", groupSizes)}");
+
+        for (int group = 0; group < numGroups; group++)
+        {
+            Console.WriteLine($"processing group of size {groupSizes[group]} ({group + 1}/{numGroups})");
+            Console.WriteLine($"Current free rooms: {string.Join(", ", freeRooms)}");
+            int assignedHotel = 0;
+
+            for (int hotel = 0; hotel < numHotels; hotel++)
+            {
+                Console.WriteLine($"availability in hotel {hotel + 1}/{numHotels} = {freeRooms[hotel]}");
+                if (groupSizes[group] <= freeRooms[hotel])
+                {
+                    assignedHotel = hotel + 1;
+                    freeRooms[hotel] -= groupSizes[group];
+                    Console.WriteLine($"assigning group {group} to hotel {hotel} which now has availability of {freeRooms[hotel]}");
+                    break;
+                }
+            }
+            assignedHotels[group] = assignedHotel;
+        }
+
+
+        return string.Join(" ", assignedHotels) + " ";
     }
 }
